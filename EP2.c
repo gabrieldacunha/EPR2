541ecb3c0b45fdbcb5ae9393848f8c8fec0fbc53
gabrieldacunha <gdcunha.r@gmail.com>
2018-07-21 01:31:08 -0300

    Tirando função inútil

--- EP2.c
+++ EP2.c
@@ -20,7 +20,6 @@
 double* criar_vetor(int N);
 int* criar_vetor_int(int N);
 double complex* criar_vetor_complexo(int N);
-void teste_complexos();
 int tamanho_arquivo(char *nome_arquivo, int *channels);
 int ler_arquivo_dat(char *nome_arquivo, int N, double complex *tempo, double complex *f, double complex *f2);
 void escrever_arquivo_dat(char *nome_arquivo, int sample_rate, int channels, int N, double complex *tempo, double complex *f, double complex *f2);
@@ -178,31 +177,6 @@ double complex* criar_vetor_complexo(int N) {
     vetor = (double complex*) malloc(N * sizeof(double complex));
 
     return vetor;
-}
-
-void teste_complexos() {
-	/* Como usar numeros complexos em C: https://stackoverflow.com/questions/6418807/how-to-work-with-complex-numbers-in-c# */
-	double complex z1 = 1.0 + 3.0 * I;
-	double complex z2 = 1.0 - 4.0 * I;
-
-	printf("Working with complex numbers:\n\v");
-
-    printf("Starting values: Z1 = %.2f + %.2fi\tZ2 = %.2f %+.2fi\n", creal(z1), cimag(z1), creal(z2), cimag(z2));
-
-    double complex sum = z1 + z2;
-    printf("The sum: Z1 + Z2 = %.2f %+.2fi\n", creal(sum), cimag(sum));
-
-    double complex difference = z1 - z2;
-    printf("The difference: Z1 - Z2 = %.2f %+.2fi\n", creal(difference), cimag(difference));
-
-    double complex product = z1 * z2;
-    printf("The product: Z1 x Z2 = %.2f %+.2fi\n", creal(product), cimag(product));
-
-    double complex quotient = z1 / z2;
-    printf("The quotient: Z1 / Z2 = %.2f %+.2fi\n", creal(quotient), cimag(quotient));
-
-    double complex conjugate = conj(z1);
-    printf("The conjugate of Z1 = %.2f %+.2fi\n", creal(conjugate), cimag(conjugate));
 }
 
 
